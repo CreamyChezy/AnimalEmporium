6b40b610fd0263596cc2049b0228ec2c686a4ae4
CreamyChezy <caesarmaurice@gmail.com>
2026-01-15 13:55:59 -0500

    added haste 2/haste 8 min pots, gave worm soup haste 2 20 sec and buffed gold worm to haste 1 20 sec

--- src/main/java/net/sean/emporium/potion/ModPotions.java
+++ src/main/java/net/sean/emporium/potion/ModPotions.java
@@ -2,6 +2,7 @@ package net.sean.emporium.potion;
 
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Items;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.Potions;
 import net.minecraft.recipe.BrewingRecipeRegistry;
@@ -13,6 +14,8 @@ import net.sean.emporium.item.ModItems;
 
 public class ModPotions {
     public static Potion HASTE_POTION;
+    public static Potion LONG_HASTE_POTION;
+    public static Potion STRONG_HASTE_POTION;
 
     private static Potion register(String name, Potion potion){
         return Registry.register(Registries.POTION, new Identifier(AnimalEmporium.MOD_ID, name), potion);
@@ -20,8 +23,12 @@ public class ModPotions {
 
     public static void registerPotions(){
         HASTE_POTION = register("haste_potion", new Potion(new StatusEffectInstance(StatusEffects.HASTE, 3600)));
+        LONG_HASTE_POTION = register("long_haste_potion", new Potion(new StatusEffectInstance(StatusEffects.HASTE,9600)));
+        STRONG_HASTE_POTION = register("strong_haste_potion", new Potion(new StatusEffectInstance(StatusEffects.HASTE,1800,1)));
     }
     public static void registerPotionsRecipes(){
-        BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, ModItems.GOLDEN_WORM, ModPotions.HASTE_POTION);
+        BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, ModItems.GOLDEN_WORM, HASTE_POTION);
+        BrewingRecipeRegistry.registerPotionRecipe(HASTE_POTION, Items.REDSTONE, LONG_HASTE_POTION);
+        BrewingRecipeRegistry.registerPotionRecipe(HASTE_POTION, Items.GLOWSTONE_DUST, STRONG_HASTE_POTION);
     }
 }
